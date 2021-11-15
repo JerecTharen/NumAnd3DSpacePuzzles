f383da3332a0a09d980a740d1316bc6e13ae6f1c
JerecTharen <kf7ags@gmail.com>
2021-11-14 23:25:00 -0500

    WIP

--- src/CubeSolver/CycleLib/CycleLib.ts
+++ src/CubeSolver/CycleLib/CycleLib.ts
@@ -14,20 +14,30 @@ export class CycleLib implements ICycleLib{
         this.coordLib = new CoordLib();
     }
     RunCycle():void{
+        /**
+         * TODO: make sure that it only does logic based off of active nodes at the start of the loop
+         * and that it does not use the new active status for nodes created vertically
+         */
+        this.EndNodes = [];
+        //Separate array from the start array that can hold inactive nodes that were not tracked in the start arrray
+        let startAndNewNodes: CoordinateNode[] = [...this.StartNodes];
         this.StartNodes.forEach((node: CoordinateNode) => {
             //Get Neighbors
             let neighborCoords: Coordinate[] = this.coordLib.GetNeighborCoords(node.coordinate);
+            let copiedNode: CoordinateNode = {...node};
 
-            neighborCoords.forEach((coord: Coordinate) => {
-                //Find or create the node
-                let node: CoordinateNode = this.coordLib.FindOrCreateNode(coord, this.StartNodes);
+            //Make sure relavant non-tracked nodes are created
+            neighborCoords.forEach((coord: Coordinate) => 
+                this.coordLib.InsertNode(this.coordLib.FindOrCreateNode(coord, startAndNewNodes), startAndNewNodes));
+            
+            let activeNeighborCount:number = neighborCoords.filter((coord: Coordinate)=> 
+                this.coordLib.FindOrCreateNode(coord, startAndNewNodes).isActive).length;
+            copiedNode = this.coordLib.DecideActiveStatus(copiedNode, activeNeighborCount);
 
-                //TODO: Determine if it should be active or inactive
-
-                //Insert
-                this.EndNodes = this.coordLib.InsertNode(node, this.EndNodes);
-            });
-            //Repeat for the rest of the original nodes
+            //Insert
+            this.EndNodes = this.coordLib.InsertNode(node, this.EndNodes);
+            //Start over and repeat for the rest of the original nodes
+            this.StartNodes = [...this.EndNodes];
         });
     }
     RunAllCycles():void{
